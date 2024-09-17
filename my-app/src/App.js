b77df85c587b68d95c5ee28c4285611e896856fa
Zainan Ali <zainanzaher09@gmail.com>
2024-09-17 11:54:22 +0100

    Removed node modules from tacking / fixed pushed bugs with credentials

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import Form from './components/form/form';
 import './App.css';
 import { Provider } from 'react-redux';
 import {store} from './app/store/store.js'
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <h1>
-          Components
-        </h1>
-        <Form />
-      </div>
+      <div>Nav Bar Goes here</div>
+      <main>
+        <Outlet/>
+      </main>
     </Provider>
   );
 }
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import App from './App';
+import Form from './components/form/form';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App/>}>
+      <Route index element={<Form />} />
+    </Route>
+  )
+);
 
 root.render(
   <React.StrictMode>
-    <RouterProvider>
-      <App />
+    <RouterProvider router={router}>
     </RouterProvider>
   </React.StrictMode>
 );
