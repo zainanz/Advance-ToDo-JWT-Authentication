f9b931d36a5f09e4596bc3f80b53c301eff67be2
Zainan Ali <zainanzaher09@gmail.com>
2024-09-21 21:45:01 +0100

    Created & Designed Add Todo Page, Fixed Routing on User Log out / login

--- test-components/config/routes.rb
+++ test-components/config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/gettodo", to: "todos#user_todos"
   post "/addtodo", to: "todos#add_todo"
+  post "/markascompleted/:id", to: "todos#mark_as_completed"
+  delete "/deletetodo/:id", to: "todos#delete_todo"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
