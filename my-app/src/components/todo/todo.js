357967632ec55b374e0da8246c859b1b52ff6e69
Zainan Ali <zainanzaher09@gmail.com>
2024-10-03 10:42:31 +0100

    Added Lazy Loading to speed up loading time

--- my-app/src/components/todo/todo.js
+++ my-app/src/components/todo/todo.js
@@ -1,17 +1,22 @@
 import "../color.css"
 import "./todo.css"
-import AddTodo from  "./components/addtodo/addtodo.js"
-import ShowTodo from "./components/showtodo/showtodo.js"
-
+import { lazy,Suspense } from "react"
+import AddTodo from "./components/addtodo/addtodo.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+const ShowTodo = lazy( () => import("./components/showtodo/showtodo.js"));
 
 
 export default function Todo(){
 
   return (
     <div>
+
       <h1 className="text-center my-5 sofadi-one-regular"> Manage your daily tasks </h1>
       <AddTodo/>
-      <ShowTodo/>
+      <Suspense fallback={<div className="w-full h-full flex justify-center items-center"><FontAwesomeIcon className="fs-3 loading-animate" icon={faSpinner} /></div>}>
+        <ShowTodo/>
+      </Suspense>
     </div>
     )
 
